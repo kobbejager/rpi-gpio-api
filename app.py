f6f66a93715c92e98bd5fbdb03435db289e45983
Wim Fremout <kobbejager@users.noreply.github.com>
2020-05-24 16:15:20 +0200

    Reverse logic (active low)

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ import RPi.GPIO as GPIO
 
 relays = {"badkamer": 8, "slaapkamer": 10}
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(relays["badkamer"], GPIO.OUT)
-GPIO.setup(relays["slaapkamer"], GPIO.OUT)
+
+GPIO.setup(relays["badkamer"], GPIO.OUT, GPIO.HIGH)
+GPIO.setup(relays["slaapkamer"], GPIO.OUT, GPIO.HIGH)
 
 app = FlaskAPI(__name__)
 
@@ -21,22 +23,32 @@ def api_root():
 def api_relay(relay):
     if request.method == "POST":
         if relay in relays:
-            GPIO.output(relays[relay], int(request.data.get("state")))
-    return {"state": GPIO.input(relays[relay])}
+            set(relay, int(request.data.get("state")))
+    return {"state": get(relay)}
 
 @app.route('/<relay>/<state>', methods=["GET"])
 def api_relay_set(relay, state):
     if state == "1" or state == "on":
-        set = 1
+        set(relay, 1)
     elif state == "0" or state == "off":
-        set = 0
+        set(relay, 0)
     elif state == "toggle":
-        set = abs(GPIO.input(relays[relay]) - 1)
+        set(relay)
     else:
         return "", status.HTTP_400_BAD_REQUEST
 
     GPIO.output(relays[relay], set)
-    return {"state": GPIO.input(relays[relay])}
+    return {"state": get(relay)}
+
+def get(relay):
+    return abs(GPIO.input(relays[relay]) - 1) #reverse logic (active low)
+
+def set(relay, value = None): #if no value given: toggle
+    if value != None:
+        value = abs(value - 1) #reverse logic (active low)
+    else:
+        value = get(relay)
+    GPIO.output(relays[relay], value)
 
 if __name__ == "__main__":
     app.run()
